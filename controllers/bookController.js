7d56d2a10212097d67c2158f22de187cbc018946
josephttran <42635012+josephttran@users.noreply.github.com>
2018-11-01 04:44:52 -0700

    update route and controller

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,4 +1,5 @@
 const { Book } = require('../models/book');
+const ObjectId = require('mongoose').Types.ObjectId;
 
 exports.bookCreate = (req, res) => {
   const book = new Book ({
@@ -26,3 +27,45 @@ exports.bookRead = (req, res) => {
     }
   })
 }
+
+exports.bookReadOne = (req, res) => {
+  if (!ObjectId.isValid(req.params.id)){
+    return res.status(400).send(`No record with given id: ${req.params.id}`);
+  }
+
+  Book.findById(req.params.id, (err, book) => {
+    if (!err) {
+      res.send(book);
+    } else {
+      console.log(`Error retriving book: ${err}`);
+    }
+  })
+};
+
+exports.bookUpdate = (req, res) => {
+  if (!ObjectId.isValid(req.params.id)){
+    return res.status(400).send(`No record with given id: ${req.params.id}`);
+  }
+
+  Book.findByIdAndUpdate(req.params.id, {$set: req.body}, {new: true}, (err, book) => {
+    if (!err) {
+      res.send(`Update Success: ${book}`);
+    } else {
+      console.log(`Error retriving book: ${err}`);
+    }
+  })
+};
+
+exports.bookDelete = (req, res) => {
+  if (!ObjectId.isValid(req.params.id)){
+    return res.status(400).send(`No record with given id: ${req.params.id}`);
+  }
+
+  Book.findByIdAndRemove(req.params.id, (err, book) => {
+    if (!err) {
+      res.send(`Delete Success: ${book}`);
+    } else {
+      console.log(`Error retriving book: ${err}`);
+    }
+  })
+};
--- routes/bookRoute.js
+++ routes/bookRoute.js
@@ -4,7 +4,9 @@ const router = express.Router();
 const bookController = require('../controllers/bookController');
 
 router.post('/create', bookController.bookCreate);
-
 router.get('/read', bookController.bookRead);
+router.get('/:id/read', bookController.bookReadOne);
+router.put('/:id/update', bookController.bookUpdate);
+router.delete('/:id/delete', bookController.bookDelete);
 
 module.exports = router;
